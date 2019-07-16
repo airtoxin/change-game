30980cbacd0c7941af87af70c8de43085d28c776
airtoxin <airtoxin@icloud.com>
2019-07-16 23:26:37 +0900

    Add test

--- src/domains/PerfectPayCalculator.test.ts
+++ src/domains/PerfectPayCalculator.test.ts
@@ -4,7 +4,7 @@ import { PerfectPayCalculator } from "./PerfectPayCalculator";
 
 describe("PerfectPayCalculator", () => {
   it("should return true when pay exact amount", () => {
-    const payMonies: Money[] = [
+    const payMonies = [
       Money(1000),
       Money(1000),
       Money(500),
@@ -17,7 +17,7 @@ describe("PerfectPayCalculator", () => {
       Money(1)
     ];
     const changeMonies: Money[] = [];
-    const walletMoniesAfterPayed: Money[] = [Money(5000), Money(1000)];
+    const walletMoniesAfterPayed = [Money(5000), Money(1000)];
 
     expect(
       new PerfectPayCalculator().isPerfectPay(
@@ -33,7 +33,7 @@ describe("PerfectPayCalculator", () => {
     // wallet = 1,1,1,10,10,10,10,1000,1000 = 2043
     // pay = 10,10,10,10,1000,1000 = 2040
     // change = 806 = 500,100,100,5,1
-    const payMonies: Money[] = [
+    const payMonies = [
       Money(1000),
       Money(1000),
       Money(10),
@@ -41,14 +41,14 @@ describe("PerfectPayCalculator", () => {
       Money(10),
       Money(10)
     ];
-    const changeMonies: Money[] = [
+    const changeMonies = [
       Money(500),
       Money(100),
       Money(100),
       Money(5),
       Money(1)
     ];
-    const walletMoniesAfterPayed: Money[] = [
+    const walletMoniesAfterPayed = [
       Money(500),
       Money(100),
       Money(100),
@@ -66,5 +66,39 @@ describe("PerfectPayCalculator", () => {
         walletMoniesAfterPayed
       )
     ).toBe(true);
+  });
+
+  it("should return false when pay is redundant", () => {
+    // price = 1234
+    // wallet = 1000,1000,500
+    // pay = 1000,1000,500
+    // change = 1266 = 1000,100,100,50,10,5,1
+    const payMonies = [Money(1000), Money(1000), Money(500)];
+    const changeMonies = [
+      Money(1000),
+      Money(100),
+      Money(100),
+      Money(50),
+      Money(10),
+      Money(5),
+      Money(1)
+    ];
+    const walletMoniesAfterPayed = [
+      Money(1000),
+      Money(100),
+      Money(100),
+      Money(50),
+      Money(10),
+      Money(5),
+      Money(1)
+    ];
+
+    expect(
+      new PerfectPayCalculator().isPerfectPay(
+        payMonies,
+        changeMonies,
+        walletMoniesAfterPayed
+      )
+    ).toBe(false);
   });
 });
